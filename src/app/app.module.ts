71e19572682226e4c930f6782a41abfedcc48a2e
Lukas <lukas.bucquoye@student.ap.be>
2019-03-24 13:51:42 +0100

    FlexGrid toegevoegd bij weergave films

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+
+import { ButtonModule } from 'primeng/button';
+import { ToolbarModule } from 'primeng/toolbar';
+import { TableModule } from 'primeng/table';
+import { PanelModule } from 'primeng/panel';
+import { InputTextModule } from 'primeng/inputtext';
 
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
@@ -15,8 +22,14 @@ import { MovieComponent } from './movie/movie.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ButtonModule,
+    ToolbarModule,
+    TableModule,
+    PanelModule,
+    InputTextModule
   ],
   providers: [
     MovieService
--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MovieService, IMovieInfo } from './movie.service';
+import { MovieService, IMovieInfo, IResult } from './movie.service';
 
 @Component({
   selector: 'app-movie',
@@ -10,6 +10,11 @@ export class MovieComponent implements OnInit {
 
   moviePosterUrl: string;
 
+  movieList: IResult[];
+  posterList: string[];
+
+  testList: string[] = ["FILM 1", "FILM 2", "FILM 3", "FILM 4"];
+
   constructor(private svc: MovieService) { }
 
   ngOnInit() {
@@ -18,15 +23,29 @@ export class MovieComponent implements OnInit {
 
   searchMovie(userInput: string){
     this.svc.getMovie(userInput).subscribe((result) => {
-      console.table(result);
+      //console.table(result);
 
-      if(result.results[0] != null){
-        this.moviePosterUrl =  'https://image.tmdb.org/t/p/w500' + result.results[0].poster_path;
-        console.log(this.moviePosterUrl);
+      this.movieList = result.results;
+      console.table(this.movieList);
+
+      //Lijst van poster url's ophalen en volledig maken
+      this.posterList = new Array(result.results.length);
+      for (let i = 0; i < result.results.length; i++) {
+        this.posterList[i] = 'https://image.tmdb.org/t/p/w300' + result.results[i].poster_path;
       }
-      else{
-        console.log("Deze film bestaat niet of er kon niets van worden teruggevonden")
-      }
+
+      // console.table(this.posterList);
+
+
+
+
+      // if(result.results[0] != null){
+      //   this.moviePosterUrl =  'https://image.tmdb.org/t/p/w500' + result.results[0].poster_path;
+      //   console.log(this.moviePosterUrl);
+      // }
+      // else{
+      //   console.log("Deze film bestaat niet of er kon niets van worden teruggevonden")
+      // }
       
     })
   }
