18e6ede8caf76563a70a8cb60b64ceb88175e4cc
Lbucquoye <lukas.bucquoye@student.ap.be>
2019-03-26 16:09:07 +0100

    getMovieInfo() en bijhorende interfaces toegevoegd

--- src/app/movie/movie.service.ts
+++ src/app/movie/movie.service.ts
@@ -8,16 +8,19 @@ export class MovieService {
 
   constructor(private http: HttpClient) { }
 
-  // getMovie(){
-  //   return this.http.get<IMovieInfo>('https://api.themoviedb.org/3/search/movie?api_key=b214031a8024721ce93ad896558c66ec&query=Jack+Reacher');
   //   //API-key: b214031a8024721ce93ad896558c66ec
-  // }
+
      getMovie(title: string = "Interstellar"){
-       return this.http.get<IMovieInfo>(`https://api.themoviedb.org/3/search/movie?api_key=b214031a8024721ce93ad896558c66ec&query=${title}`)
+       return this.http.get<IMovie>(`https://api.themoviedb.org/3/search/movie?api_key=b214031a8024721ce93ad896558c66ec&query=${title}`)
+     }
+
+     getMovieInfo(Id: string){
+       return this.http.get<IMovieInfo>(`https://api.themoviedb.org/3/movie/${Id}?api_key=b214031a8024721ce93ad896558c66ec`)
      }
 
 }
 
+// getMovie
 export interface IResult {
   vote_count: number;
   id: number;
@@ -35,9 +38,67 @@ export interface IResult {
   release_date: string;
 }
 
-export interface IMovieInfo {
+export interface IMovie {
   page: number;
   total_results: number;
   total_pages: number;
   results: IResult[];
+}
+
+// getMovieInfo
+export interface IBelongsToCollection {
+  id: number;
+  name: string;
+  poster_path: string;
+  backdrop_path: string;
+}
+
+export interface IGenre {
+  id: number;
+  name: string;
+}
+
+export interface IProductionCompany {
+  id: number;
+  logo_path: string;
+  name: string;
+  origin_country: string;
+}
+
+export interface IProductionCountry {
+  iso_3166_1: string;
+  name: string;
+}
+
+export interface ISpokenLanguage {
+  iso_639_1: string;
+  name: string;
+}
+
+export interface IMovieInfo {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: IBelongsToCollection;
+  budget: number;
+  genres: IGenre[];
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: IProductionCompany[];
+  production_countries: IProductionCountry[];
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: ISpokenLanguage[];
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
 }
