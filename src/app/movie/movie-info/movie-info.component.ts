ae8350bb64b249d8dc21d12b28a4609734d13007
Lukas <lukas.bucquoye@student.ap.be>
2019-03-29 16:56:47 +0100

    Check of er movieId is

--- src/app/movie/movie-info/movie-info.component.ts
+++ src/app/movie/movie-info/movie-info.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { MovieinfoService } from '../../services/movieinfo.service';
 import { MovieService, IMovieInfo, IGenre } from 'src/app/services/movie.service';
+import { RouterLink, Router } from '@angular/router';
 
 @Component({
   selector: 'app-movie-info',
@@ -20,28 +21,32 @@ export class MovieInfoComponent implements OnInit {
   imdbLink: string;
   genres: IGenre[];
 
-  constructor(private sharedSvc: MovieinfoService, private movieSvc: MovieService) { }
+  constructor(private sharedSvc: MovieinfoService, private movieSvc: MovieService, private router: Router) { }
 
   ngOnInit() {
     this.movieId = this.sharedSvc.getId();
-    // TODO: Als this.sharedSvc.getId() leeg is moet er terug gegaan worden naar de search pagina
-    console.log(`Het id van deze film is: ${this.sharedSvc.getId()}`);
-
-    this.movieSvc.getMovieInfo(this.movieId).subscribe((result) => {
-      this.movieInfo = result;
-      console.table(this.movieInfo);
-      this.poster_full_url = this.poster_base_url + this.movieInfo.poster_path;
-      this.backdrop = this.poster_base_url + this.movieInfo.backdrop_path;
-      this.movieTitle = this.movieInfo.original_title;
-      this.budget = this.movieInfo.budget.toString();
-
-      //TODO: Imdb link verder uitwerken als dat gaat
-      this.imdbLink = this.movieInfo.imdb_id;
-
-      this.genres = this.movieInfo.genres;
-    })
-
     
-  }
+    //Als er geen movie id is (geen film geselecteerd) wordt er terug naar de search pagina verwezen
+    if(this.movieId == ""){
+      this.router.navigate(['search']);
+      console.log("geen id gevonden, terug naar search!");
+    }
+    else{
+      console.log(`Het id van deze film is: ${this.sharedSvc.getId()}`);
 
+      this.movieSvc.getMovieInfo(this.movieId).subscribe((result) => {
+        this.movieInfo = result;
+        console.table(this.movieInfo);
+        this.poster_full_url = this.poster_base_url + this.movieInfo.poster_path;
+        this.backdrop = this.poster_base_url + this.movieInfo.backdrop_path;
+        this.movieTitle = this.movieInfo.original_title;
+        this.budget = this.movieInfo.budget.toString();
+  
+        //TODO: Imdb link verder uitwerken als dat gaat
+        this.imdbLink = this.movieInfo.imdb_id;
+  
+        this.genres = this.movieInfo.genres;
+      })
+    }  
+  }
 }
