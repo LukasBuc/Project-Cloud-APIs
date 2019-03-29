ac9667089924bd509be5598ecd490b70492afeb9
Lukas <lukas.bucquoye@student.ap.be>
2019-03-29 13:09:32 +0100

    Router functionaliteit toegevoegd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,13 @@ import { InputTextModule } from 'primeng/inputtext';
 
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
-import { MovieService } from './movie/movie.service';
+import { MovieService } from './services/movie.service';
 import { MovieComponent } from './movie/movie.component';
-import { MovieInfoComponent } from './movie-info/movie-info.component';
-import { MovieinfoService } from './movie-info/movieinfo.service';
+import { MovieInfoComponent } from './movie/movie-info/movie-info.component';
+import { MovieinfoService } from './services/movieinfo.service';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,12 @@ import { MovieinfoService } from './movie-info/movieinfo.service';
     ToolbarModule,
     TableModule,
     PanelModule,
-    InputTextModule
+    InputTextModule,
+    RouterModule.forRoot([
+      { path: "search", component: MovieComponent},
+      { path: "info", component: MovieInfoComponent},
+      { path: "", redirectTo: "search", pathMatch: "full"}
+    ])
   ],
   providers: [
     MovieService,
